826f531cde275f11b558ba04cf590bc5b6d679ea
Theodor Urtegard <regine.urtegard@no.experis.com>
2020-10-17 16:48:10 +0200

    Initial commit

---
+++ signalr/Server/Controllers/Notifications.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using signalr.Server.Models;
+
+namespace signalr.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Notifications : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public Notifications(IHubContext<NotificationHub> hubContext) 
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] SimpleTitleString simpleTitleString)
+        {
+            await _hubContext.Clients.All.SendAsync("notification", $"{DateTime.Now} : {simpleTitleString.Title}");
+            return Ok("Notification has been sent successfully!");
+        }
+    }
+}
---
+++ signalr/Server/Models/SimpleTitleString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalr.Server.Models
+{
+    public class SimpleTitleString
+    {
+        public string Title { get; set; }
+    }
+}
